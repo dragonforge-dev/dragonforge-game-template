50095f391ffa7f3b6a6a3cbe968aaa15c7329e87
Dragonforge Development <dougbnoel@gmail.com>
2025-09-26 19:51:33 -0600

    Implemented restarting pause music.

--- addons/dragonforge_game_template/game_states/game_state_main_menu.gd
+++ addons/dragonforge_game_template/game_states/game_state_main_menu.gd
@@ -2,6 +2,8 @@ class_name GameStateMainMenu extends State
 
 ## Music that plays whenever the main menu is visible.
 @export var main_menu_music: Song
+## The number of seconds after the song ends before it starts again.
+@export_range(0.0, 300.0, 0.1, "suffix:seconds") var replay_countdown_time: float = 30.0
 ## The background to display whenever the main menu is visible.
 @export var background: Control
 
@@ -11,6 +13,7 @@ class_name GameStateMainMenu extends State
 func _activate_state() -> void:
 	super()
 	Game.splash_screens_complete.connect(switch_state)
+	Music.pause_song_finished.connect(_on_pause_song_finished)
 	set_process_input(true)
 	user_interface.hide_ui()
 	background.hide()
@@ -38,3 +41,8 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		switch_state()
 		get_viewport().set_input_as_handled()
+
+
+func _on_pause_song_finished() -> void:
+	await get_tree().create_timer(replay_countdown_time).timeout
+	main_menu_music.play()
