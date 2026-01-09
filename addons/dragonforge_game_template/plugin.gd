ead2ee96d64c38e985a5a0fdaa6b493ca8415fc7
Dragonforge Development <dougbnoel@gmail.com>
2026-01-09 16:00:30 -0700

    Fixed bug where the plugin tries to set the renamed main-template.tscn as the main scene. It is now called main.tscn.

--- addons/dragonforge_game_template/plugin.gd
+++ addons/dragonforge_game_template/plugin.gd
@@ -8,7 +8,7 @@ const SKIP_ACTION = "skip"
 const PAUSE_ACTION = "pause"
 const MAIN_SCENE_SETTING = "application/run/main_scene"
 const DEFAULT_AUTOLOAD_GAME_SCENE = "res://addons/dragonforge_game_template/game_base.tscn"
-const DEFAULT_AUTOLOAD_MAIN_SCENE = "res://addons/dragonforge_game_template/main_template.tscn"
+const DEFAULT_AUTOLOAD_MAIN_SCENE = "res://addons/dragonforge_game_template/main.tscn"
 
 
 func _enable_plugin() -> void:
